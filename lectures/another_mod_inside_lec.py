39c2e2df45f0b9c7dce7470ff938eaca8dc57daf
XinyueMao8 <z5273650@ad.unsw.edu.au>
2023-10-29 02:19:08 +1100

    add lectures in repo

---
+++ lectures/another_mod_inside_lec.py
@@ -0,0 +1,13 @@
+""" another_mod_inside_lec.py
+
+Another module inside the `lectures` package
+"""
+
+# OLD: import mod_inside_lec
+
+# import mod_inside_lec
+# this one cannot run in python console by >>> from lectures import another_mod_inside_lec
+
+from lectures import mod_inside_lec
+# this one can run in Python Console by >>> from lectures import another_mod_inside_lec
+
